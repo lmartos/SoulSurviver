da6e5e13f559ea96ddbc1187836577a768ce5c96
lmartos <l-martos@hotmail.com>
2015-03-18 15:11:45 +0100

    drawable objecten binnen de inventory

--- src/nl/spookystoriesinc/view/InventoryView.java
+++ src/nl/spookystoriesinc/view/InventoryView.java
@@ -1,20 +1,23 @@
 package nl.spookystoriesinc.view;
 
-import java.util.Observable;
+import java.util.ArrayList;
 
+import java.util.Observable;
 import java.util.Observer;
 
+import nl.spookystoriesinc.model.GameObject;
 import nl.spookystoriesinc.spookystories.R;
 import android.content.Context;
 import android.graphics.Color;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.View.OnClickListener;
 import android.widget.LinearLayout;
 
 public class InventoryView extends LinearLayout implements Observer {
+	
+	
+	private ArrayList<GameObject> items = new ArrayList<GameObject>();
 	
 	private InventoryViewTile tile1;
 	private InventoryViewTile tile2;
@@ -52,6 +55,43 @@ public class InventoryView extends LinearLayout implements Observer {
 		// TODO Auto-generated constructor stub
 	}
 	
+	public void addItemToInventory(GameObject item){
+		items.add(item);
+		switch(items.lastIndexOf(item)){
+		case 0: tile1.setImageResource(R.drawable.leaf);
+		break;
+		case 1: tile2.setImageResource(R.drawable.leaf);
+		break;
+		case 2: tile3.setImageResource(R.drawable.leaf);
+		break;
+		case 3: tile4.setImageResource(R.drawable.leaf);
+		break;
+		case 4: tile5.setImageResource(R.drawable.leaf);
+		break;
+		case 5: tile6.setImageResource(R.drawable.leaf);
+		break;
+		case 6: tile7.setImageResource(R.drawable.leaf);
+		break;
+		case 7: tile8.setImageResource(R.drawable.leaf);
+		break;
+		case 8: tile9.setImageResource(R.drawable.leaf);
+		break;
+		case 9: tile10.setImageResource(R.drawable.leaf);
+		break;
+		case 10: tile11.setImageResource(R.drawable.leaf);
+		break;
+		case 11: tile12.setImageResource(R.drawable.leaf);
+		break;
+		}
+		
+		
+		
+	}
+	
+	public void removeItemFromInventory(GameObject item){
+		items.remove(item);
+	}
+	
 	
 
 	public void init(Context context){
@@ -59,6 +99,7 @@ public class InventoryView extends LinearLayout implements Observer {
 		
 			LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 			inflater.inflate(R.layout.inventory, this);
+			
 			tile1 = (InventoryViewTile) findViewById(R.id.inventoryViewTile1);
 			tile2 = (InventoryViewTile) findViewById(R.id.inventoryViewTile2);
 			tile3 = (InventoryViewTile) findViewById(R.id.inventoryViewTile3);
@@ -71,7 +112,9 @@ public class InventoryView extends LinearLayout implements Observer {
 			tile10 = (InventoryViewTile) findViewById(R.id.inventoryViewTile10);
 			tile11 = (InventoryViewTile) findViewById(R.id.inventoryViewTile11);
 			tile12 = (InventoryViewTile) findViewById(R.id.inventoryViewTile12);
+			
 			OnClickListener onclickListener = new ClickListener();
+			
 			tile1.setOnClickListener(onclickListener);
 			tile2.setOnClickListener(onclickListener);
 			tile3.setOnClickListener(onclickListener);
@@ -84,6 +127,7 @@ public class InventoryView extends LinearLayout implements Observer {
 			tile10.setOnClickListener(onclickListener);
 			tile11.setOnClickListener(onclickListener);
 			tile12.setOnClickListener(onclickListener);
+			
 			tile1.setBackgroundResource(R.drawable.inventoryslot);
 			tile2.setBackgroundResource(R.drawable.inventoryslot);
 			tile3.setBackgroundResource(R.drawable.inventoryslot);
@@ -97,9 +141,6 @@ public class InventoryView extends LinearLayout implements Observer {
 			tile11.setBackgroundResource(R.drawable.inventoryslot);
 			tile12.setBackgroundResource(R.drawable.inventoryslot);
 			
-			
-			
-		
 		}
 		
 	}
