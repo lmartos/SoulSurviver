689765214658481210fae442ddd8c709f9f129b8
lmartos <l-martos@hotmail.com>
2015-03-18 18:10:15 +0100

    boek opent alleen als speler ernaast staat

--- src/nl/spookystoriesinc/coolgame/objects/Book.java
+++ src/nl/spookystoriesinc/coolgame/objects/Book.java
@@ -15,6 +15,7 @@ public class Book extends GameObject{
 
 	public static final String BOOK_IMAGE = "Book";
 	private ArrayList<Integer> pages = new ArrayList<Integer>();
+	private int bookX, bookY;
 	
 	public Book(int page){
 		super();
@@ -34,11 +35,33 @@ public class Book extends GameObject{
 
 	@Override
 	public void onTouched(GameBoard gameBoard) {
+		this.bookX = this.getPositionX();
+		this.bookY = this.getPositionY();
+		if(bookX == (gameBoard.getPlayer().getPositionX() + 1) && bookY == gameBoard.getPlayer().getPositionY()){
 		Intent intent = new Intent(MainActivity.getContext(), TekstOverlayActivity.class);
 		intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 		intent.putIntegerArrayListExtra("pages", pages);
 		Context context = MainActivity.getContext();
 		context.startActivity(intent);
+		}else if(bookX == (gameBoard.getPlayer().getPositionX() - 1) && bookY == gameBoard.getPlayer().getPositionY()){
+			Intent intent = new Intent(MainActivity.getContext(), TekstOverlayActivity.class);
+			intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+			intent.putIntegerArrayListExtra("pages", pages);
+			Context context = MainActivity.getContext();
+			context.startActivity(intent);
+		}else if(bookY == (gameBoard.getPlayer().getPositionY() + 1) && bookX == gameBoard.getPlayer().getPositionX()){
+			Intent intent = new Intent(MainActivity.getContext(), TekstOverlayActivity.class);
+			intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+			intent.putIntegerArrayListExtra("pages", pages);
+			Context context = MainActivity.getContext();
+			context.startActivity(intent);
+		}else if(bookY == (gameBoard.getPlayer().getPositionY() - 1) && bookX == gameBoard.getPlayer().getPositionX()){
+			Intent intent = new Intent(MainActivity.getContext(), TekstOverlayActivity.class);
+			intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+			intent.putIntegerArrayListExtra("pages", pages);
+			Context context = MainActivity.getContext();
+			context.startActivity(intent);
+		}
 		
 		
 	}
