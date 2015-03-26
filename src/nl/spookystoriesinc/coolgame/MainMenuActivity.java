029f21115dfc05cd9cda6bf09c61ba4a47933962
lmartos <l-martos@hotmail.com>
2015-03-26 10:01:51 +0100

    mainmenu v2

--- src/nl/spookystoriesinc/coolgame/MainMenuActivity.java
+++ src/nl/spookystoriesinc/coolgame/MainMenuActivity.java
@@ -3,10 +3,12 @@ package nl.spookystoriesinc.coolgame;
 import nl.spookystoriesinc.spookystories.R;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class MainMenuActivity extends Activity {
 	
@@ -27,7 +29,16 @@ public class MainMenuActivity extends Activity {
 			
 			
 		}
+	
 		
+	}
+	
+	public boolean onContectItemSelected(MenuItem item){
+		return super.onContextItemSelected(item);
+	}
+	
+	public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo){
+		super.onCreateContextMenu(menu, v, menuInfo);
 	}
 
 }
