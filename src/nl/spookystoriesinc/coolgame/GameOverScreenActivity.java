c955be9e12f8edb22b55fe8cef8a8c29588e0d2d
lmartos <l-martos@hotmail.com>
2015-04-08 21:02:33 +0200

    gameover screen fix

--- src/nl/spookystoriesinc/coolgame/GameOverScreenActivity.java
+++ src/nl/spookystoriesinc/coolgame/GameOverScreenActivity.java
@@ -3,6 +3,8 @@ package nl.spookystoriesinc.coolgame;
 import nl.spookystoriesinc.spookystories.R;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -13,6 +15,8 @@ import android.widget.RelativeLayout;
 public class GameOverScreenActivity extends Activity {
 	private Button mainMenuButton;
 	private Intent mainMenu;
+	private RelativeLayout background;
+	
 	
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
@@ -21,14 +25,18 @@ public class GameOverScreenActivity extends Activity {
 		onClickMainMenu menuClicks = new onClickMainMenu();
 		mainMenuButton = (Button) findViewById(R.id.mainMenuButton);
 		mainMenuButton.setOnClickListener(menuClicks);
-		RelativeLayout background = (RelativeLayout) findViewById(R.id.RelativeLayout1);
+		background = (RelativeLayout) findViewById(R.id.backgroundLayout1);
 		Intent intent = getIntent();
 		
 		boolean isVictory = intent.getBooleanExtra("Victory", false);
 		
+		
+		
 		if(isVictory){
+		
 			background.setBackgroundResource(R.drawable.awesome_boekenkast);
 		}else{
+			
 			background.setBackgroundResource(R.drawable.game_over_screen);
 		}
 		
@@ -38,10 +46,15 @@ public class GameOverScreenActivity extends Activity {
 
 		@Override
 		public void onClick(View arg0) {
+			Intent i = getBaseContext().getPackageManager().getLaunchIntentForPackage( getBaseContext().getPackageName() );
+		i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+		startActivity(i);
 			finish();
 		}
 
 	}
+	
+	
 	
 
 }
